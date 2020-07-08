9e79ab02feefdb910e3336893ab96aa4b2058efe
André Schmidt <andre@familischmidt.online>
2020-07-08 16:55:33 +0200

    Folder structure changed

--- patient-management-api/src/main/scala/de/heilpraktikerelbmarsch/patient/api/services/PatientService.scala
+++ patient-management-api/src/main/scala/de/heilpraktikerelbmarsch/patient/api/services/PatientService.scala
@@ -3,22 +3,22 @@ package de.heilpraktikerelbmarsch.patient.api.services
 import akka.NotUsed
 import com.lightbend.lagom.scaladsl.api.transport.{Method, NotFound}
 import com.lightbend.lagom.scaladsl.api.{Descriptor, Service, ServiceAcl, ServiceCall}
+import de.heilpraktikerelbmarsch.patient.api.adt.PatientView
 import de.heilpraktikerelbmarsch.patient.api.request.PatientSearchForm
-import de.heilpraktikerelbmarsch.patient.api.response.PatientView
 import de.heilpraktikerelbmarsch.util.adt.security.MicroServiceIdentifier.PatientServiceIdentifier
 
 trait PatientService extends Service {
 
   /**
    * Creates a new patient
-   * @return a [[de.heilpraktikerelbmarsch.patient.api.response.PatientView]]
+   * @return a [[PatientView]]
    */
   def createPatient(): ServiceCall[NotUsed,PatientView]
 
   /**
    * Get a patient by his patient number which is unique!
    * @param number patient number unique
-   * @return returns a [[de.heilpraktikerelbmarsch.patient.api.response.PatientView]] in case of found
+   * @return returns a [[PatientView]] in case of found
    *         otherwise a [[com.lightbend.lagom.scaladsl.api.transport.NotFound]]
    */
   def getPatientByPatientNumber(number: String): ServiceCall[NotUsed,PatientView]
@@ -27,7 +27,7 @@ trait PatientService extends Service {
    * Searches for patients
    * @param take maximum number of results
    * @param drop drops the first n results
-   * @return a sequence of [[de.heilpraktikerelbmarsch.patient.api.response.PatientView]]. Size 0 if nothing was found
+   * @return a sequence of [[PatientView]]. Size 0 if nothing was found
    */
   def searchPatient(take: Int, drop: Int): ServiceCall[PatientSearchForm,Seq[PatientView]]
 
